931b83d0b0957e5488ccbc66bd673bd86ef48f06
rossyvee <roseakinyi001@gmail.com>
2022-04-18 21:45:36 +0300

    adding imports for apiservice formgroup and environment to the workspace

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-import {environment} from "../environments/environment"
-import { ApiService } from './services/api.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,66 +7,6 @@ import { ApiService } from './services/api.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Github username search';
-
-  searchResults:any[] =[]
-
-  displayResponse:boolean = false
-
-  responseMessage:string = "Searching ...... kindly wait."
-
-  ngInit(){
-  }
-
-  constructor(private apiService: ApiService){
-    console.log(environment.apiUrl);
-  }
-
-  searchForm = new FormGroup({
-    username: new FormControl('',[Validators.required])
-  })
-
-/*   
-  arrow functions
-  let functionName = ()=>{
-
-  }
-
-  normal function declaration
-
-  function functionName(){
-
-  }
-
-*/
-
-  onSubmit(){
-    const username = this.searchForm.value.username
-    this.displayResponse=true
-    this.responseMessage = "Searching ...... kindly wait."
-    this.apiService.searchUsername(username).subscribe({
-      next:(data)=>{
-        
-        this.searchResults= data.items
-
-        if(data.items && data.items.length >0 ){
-          this.displayResponse =false
-        }else{
-          this.responseMessage = `No results found for username :  ${username}` 
-        }
-
-        console.log(data.items)
-      },
-      error:(error)=>{
-        console.log(error)
-      }
-    })
-
-    this.searchForm.reset()
-    console.log(this.searchForm.value);
-    
-  }
-
-
+  
 
 }
